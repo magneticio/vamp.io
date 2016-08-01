4843bdbb31cb89cd8bfdd64f64556554f049cca8
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-08-02 01:08:24 +0200

    version selection css/js

---
+++ static/javascripts/dropdown.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function version() {
+    document.getElementById("version").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
