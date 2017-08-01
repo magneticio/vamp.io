7a0303365bbdfc304324599afd1de9c16cf7e675
Tim Nolet <tim@unumotors.com>
2017-08-01 15:45:25 +0200

    use native Travis -> Github pages deploy

--- scripts/deploy_github.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$GITHUB_TOKEN" ] && [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
-echo "pushing content to gh-pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/magneticio/vamp.io.git" master:gh-pages > /dev/null 2>&1
-fi
